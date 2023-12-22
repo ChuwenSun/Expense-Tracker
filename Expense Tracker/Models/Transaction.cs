3a8f2508373d31dedc3ce866e6e1416dd3155bcd
ChuwenSun <chuwen.sun@yahoo.com>
2023-12-22 14:24:46 -0500

    Add project files.

---
+++ Expense Tracker/Models/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+    
+        //CategoryId
+        public int CategotyId { get; set; }
+        public Category Category { get; set; }
+
+        public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
+        public String? Note { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+
+    }
+}
